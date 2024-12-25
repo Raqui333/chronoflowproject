a6e7c64ed35d8fd8165651294ed1a651df793860
Raqui333 <klebersilva2025@gmail.com>
2024-12-25 00:10:19 -0300

    test: add end-to-end tests for user management endpoints

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -5,6 +5,14 @@ import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let test_user_id;
+
+  const new_user = {
+    name: 'John Doe',
+    username: 'johndoe',
+    email: 'john.doe@example.com',
+    password: 'password123',
+  };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,7 +23,51 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(404);
+  describe('/users', () => {
+    it('/ (GET) should return all users', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/users')
+        .expect(200);
+
+      expect(response.body.length).toBeGreaterThan(0);
+    });
+
+    it('/ (POST) should add a user to database', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send(new_user)
+        .expect(201);
+
+      test_user_id = response.body.id;
+
+      expect(response.body.username).toEqual(new_user.username);
+    });
+
+    it('/:id (GET) should return one user', async () => {
+      const response = await request(app.getHttpServer())
+        .get(`/users/${test_user_id}`)
+        .expect(200);
+
+      expect(response.body.id).toEqual(test_user_id);
+    });
+
+    it('/:id (PATCH) should updates one user data', async () => {
+      const response = await request(app.getHttpServer())
+        .patch(`/users/${test_user_id}`)
+        .send({
+          username: 'johndoe123',
+        })
+        .expect(200);
+
+      expect(response.body.username).toEqual('johndoe123');
+    });
+
+    it('/:id (DELETE) should delete one user from database', async () => {
+      const response = await request(app.getHttpServer())
+        .delete(`/users/${test_user_id}`)
+        .expect(200);
+
+      expect(response.body.id).toEqual(test_user_id);
+    });
   });
 });
