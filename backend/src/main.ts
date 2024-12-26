74f4d27c5ac77b1bab7b18d03be6da9270320a10
Raqui333 <klebersilva2025@gmail.com>
2024-12-26 17:25:00 -0300

    test commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,12 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
     }),
   );
+
   await app.listen(process.env.PORT || 5000);
 }
 
