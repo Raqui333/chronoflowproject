d4e6e83622d6a436d7dd7636e2c1f050c3845049
Raqui333 <klebersilva2025@gmail.com>
2024-12-29 19:12:44 -0300

    refactor: reorganize Redux files and add ReduxProvider component

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import counterReducer from './features/counter/counterSlice';
+import themeReducer from './features/theme/themeSlice';
 
 export function makeStore() {
   return configureStore({
     reducer: {
       counter: counterReducer,
+      theme: themeReducer,
     },
   });
 }
